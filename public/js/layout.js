facb5cc9034938e4fe25771f60a5d8af632c1710
Vicente <vicegd@gmail.com>
2026-04-23 13:24:42 +0200

    ✨ feat(blog): enhance blog page content and navigation
    - update blog page title and header for better visibility
    - modify blog description to clarify future content generation
    - change blog link in navigation to absolute path for consistency

--- public/js/layout.js
+++ public/js/layout.js
@@ -21,7 +21,7 @@
 			+ "\t\t</a>"
 			+ "\t\t<nav class=\"nav-links\" aria-label=\"Main navigation\">"
 			+ "\t\t\t<a class=\"" + isActive("index.html") + "\" href=\"index.html\"><span class=\"nav-label\"><span class=\"ui-icon icon-home\" aria-hidden=\"true\"></span>Home</span></a>"
-			+ "\t\t\t<a class=\"" + isBlogActive() + "\" href=\"blog/\"><span class=\"nav-label\"><span class=\"ui-icon icon-summary\" aria-hidden=\"true\"></span>Blog</span></a>"
+			+ "\t\t\t<a class=\"" + isBlogActive() + "\" href=\"/blog/\"><span class=\"nav-label\"><span class=\"ui-icon icon-summary\" aria-hidden=\"true\"></span>Blog</span></a>"
 			+ "\t\t\t<a class=\"" + isActive("releases.html") + "\" href=\"releases.html\"><span class=\"nav-label\"><span class=\"ui-icon icon-release\" aria-hidden=\"true\"></span>Releases</span></a>"
 			+ "\t\t\t<a class=\"" + isActive("changelog.html") + "\" href=\"changelog.html\"><span class=\"nav-label\"><span class=\"ui-icon icon-changelog\" aria-hidden=\"true\"></span>Changelog</span></a>"
 			+ "\t\t\t<a class=\"" + isActive("about.html") + "\" href=\"about.html\"><span class=\"nav-label\"><span class=\"ui-icon icon-about\" aria-hidden=\"true\"></span>About</span></a>"
